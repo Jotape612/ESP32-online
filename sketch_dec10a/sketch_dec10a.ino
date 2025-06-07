590904d3b49a3d0fce9149ddbd422e639e93af22
João Paulo Oliveira <158469595+Jotape612@users.noreply.github.com>
2025-06-07 12:29:08 -0300

    Update sketch_dec10a.ino

--- sketch_dec10a/sketch_dec10a.ino
+++ sketch_dec10a/sketch_dec10a.ino
@@ -1,70 +1,270 @@
+/*
+   -------------------------------------------------------------------------------------
+   HX711_ADC
+   Arduino library for HX711 24-Bit Analog-to-Digital Converter for Weight Scales
+   Olav Kallhovd sept2017
+   -------------------------------------------------------------------------------------
+*/
+
+/*
+   This example file shows how to calibrate the load cell and optionally store the calibration
+   value in EEPROM, and also how to change the value manually.
+   The result value can then later be included in your project sketch or fetched from EEPROM.
+
+   To implement calibration in your project sketch the simplified procedure is as follow:
+       LoadCell.tare();
+       //place known mass
+       LoadCell.refreshDataSet();
+       float newCalibrationValue = LoadCell.getNewCalibration(known_mass);
+*/
+
 #include <WiFi.h>
 #include <IOXhop_FirebaseESP32.h>
 #include <ArduinoJson.h>
-#include <HX711.h>
 
-#define WIFI_SSID "Bel infoneT_Apolonio"
-#define WIFI_PASSWORD "marcador123@"
+
+#define WIFI_SSID "SENAI_ACADEMICO"
+#define WIFI_PASSWORD "Senai*Academico"
 #define FIREBASE_HOST "https://esp32-6fe25-default-rtdb.firebaseio.com/"
 #define FIREBASE_AUTH "bI68m0iE1rOM0zij1b6VSwtQ14r6YZ3K747dfdkN"
 
-#define LEDR 13
-#define LEDG 12
-#define LEDY 14
-#define DT_LC 27
-#define SCK_LC 14
-#define PESO_MAXIMO 50.0
 
-HX711 scale;
+#include <HX711_ADC.h>
+#if defined(ESP8266)|| defined(ESP32) || defined(AVR)
+#include <EEPROM.h>
+#endif
+
+void calibrate();
+void changeSavedCalFactor();
+
+//buffer
+const int numReadings = 10;  // Número de leituras para a média
+float readings[numReadings];  // Array para armazenar as leituras
+int readIndex = 0;            // Índice da leitura atual
+float total = 0;              // Soma das leituras
+float average = 0;            // Média calculada
+
+//pins:
+const int HX711_dout = 14; //mcu > HX711 dout pin
+const int HX711_sck = 12; //mcu > HX711 sck pin
+
+//HX711 constructor:
+HX711_ADC LoadCell(HX711_dout, HX711_sck);
+
+const int calVal_eepromAdress = 0;
+unsigned long lastPrintTime = 0;
+const unsigned long printInterval = 5000; // 5 segundos
 
 void setup() {
-  Serial.begin(115200);
+
+  Serial.begin(57600); delay(10);
   Serial.println();
+  Serial.println("Starting...");
 
-  // Conexão WiFi
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Conectando ao WiFi");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(300);
+
+  for (int i = 0; i < numReadings; i++) {
+    readings[i] = 0;
   }
-  Serial.println("\nWiFi conectado!");
 
+  LoadCell.begin();
+  //LoadCell.setReverseOutput(); //uncomment to turn a negative output value to positive
+  unsigned long stabilizingtime = 2000; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
+  boolean _tare = true; //set this to false if you don't want tare to be performed in the next step
+  LoadCell.start(stabilizingtime, _tare);
+  if (LoadCell.getTareTimeoutFlag() || LoadCell.getSignalTimeoutFlag()) {
+    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
+    while (1);
+  }
+  else {
+    LoadCell.setCalFactor(1.0); // user set calibration value (float), initial value 1.0 may be used for this sketch
+    Serial.println("Startup is complete");
+  }
+  while (!LoadCell.update());
+  calibrate(); //start calibration procedure
+
+  // Conecta ao WiFi
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print("Conectando ao WiFi");
+    while (WiFi.status() != WL_CONNECTED) {
+  delay(500);
+  Serial.print(".");
+  }
+  Serial.println(" Conectado!");
+
+  // Inicializa o Firebase
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-
-  scale.begin(DT_LC, SCK_LC);
-  scale.set_scale(470); 
-  scale.tare();
-
-  pinMode(LEDR, OUTPUT);
-  pinMode(LEDG, OUTPUT);
-  pinMode(LEDY, OUTPUT);
 }
 
 void loop() {
-  float pesoAtual = scale.get_units(10); 
-  if (pesoAtual < 0) pesoAtual = 0; 
+    static boolean newDataReady = 0;
+    const int serialPrintInterval = 100;  // Aumente para reduzir a frequência de impressão
 
-  int porcentagem = (pesoAtual / PESO_MAXIMO) * 100;
-  porcentagem = constrain(porcentagem, 0, 100);
+    if (LoadCell.update()) newDataReady = true;
 
-  Firebase.setFloat("/Prateleira/pesoAtual", pesoAtual);
-  Firebase.setInt("/Prateleira/porcentagem", porcentagem);
+if (newDataReady) {
+    float i = LoadCell.getData();
+                // Subtrai a última leitura do total
+            total = total - readings[readIndex];
+            
+            // Lê o novo valor e adiciona ao total
+            readings[readIndex] = i;
+            total = total + readings[readIndex];
+            
+            // Avança para a próxima posição no array
+            readIndex = (readIndex + 1) % numReadings;
+            
+            // Calcula a média
+average = (total / numReadings); 
+}
 
+// Exibe apenas a cada 5 segundos
+if (millis() - lastPrintTime >= printInterval) {
+  float pesoCorrigido = average; // Já está dividido por 100
+  
+  Serial.print("Peso médio ("); 
+  Serial.print(numReadings);
+  Serial.print(" leituras): ");
+  Serial.print(pesoCorrigido, 2); // Mostra com 2 casas decimais
+  Serial.println("g"); // Adiciona "g" para indicar gramas
+  
+  Firebase.setFloat("/Prateleira/A1/pesoAtual", pesoCorrigido);
+  Serial.println("Dados enviados para Firebase");
+  
+  lastPrintTime = millis();
+}
+    // Restante do código (comandos 't', 'r', 'c')...
+}
 
-  if (porcentagem <= 30) {
-    digitalWrite(LEDR, HIGH);
-    digitalWrite(LEDY, LOW);
-    digitalWrite(LEDG, LOW);
-  } else if (porcentagem <= 70) {
-    digitalWrite(LEDR, LOW);
-    digitalWrite(LEDY, HIGH);
-    digitalWrite(LEDG, LOW);
-  } else {
-    digitalWrite(LEDR, LOW);
-    digitalWrite(LEDY, LOW);
-    digitalWrite(LEDG, HIGH);
+void calibrate() {
+  Serial.println("***");
+  Serial.println("Start calibration:");
+  Serial.println("Place the load cell an a level stable surface.");
+  Serial.println("Remove any load applied to the load cell.");
+  Serial.println("Send 't' from serial monitor to set the tare offset.");
+
+  boolean _resume = false;
+  while (_resume == false) {
+    LoadCell.update();
+    if (Serial.available() > 0) {
+      if (Serial.available() > 0) {
+        char inByte = Serial.read();
+        if (inByte == 't') LoadCell.tareNoDelay();
+      }
+    }
+    if (LoadCell.getTareStatus() == true) {
+      Serial.println("Tare complete");
+      _resume = true;
+    }
   }
 
-  delay(3000);
+  Serial.println("Now, place your known mass on the loadcell.");
+  Serial.println("Then send the weight of this mass (i.e. 100.0) from serial monitor.");
+
+  float known_mass = 0;
+  _resume = false;
+  while (_resume == false) {
+    LoadCell.update();
+    if (Serial.available() > 0) {
+      known_mass = Serial.parseFloat();
+      if (known_mass != 0) {
+        Serial.print("Known mass is: ");
+        Serial.println(known_mass);
+        _resume = true;
+      }
+    }
+  }
+
+  LoadCell.refreshDataSet(); //refresh the dataset to be sure that the known mass is measured correct
+  float newCalibrationValue = LoadCell.getNewCalibration(known_mass); //get the new calibration value
+
+  Serial.print("New calibration value has been set to: ");
+  Serial.print(newCalibrationValue);
+  Serial.println(", use this as calibration value (calFactor) in your project sketch.");
+  Serial.print("Save this value to EEPROM adress ");
+  Serial.print(calVal_eepromAdress);
+  Serial.println("? y/n");
+
+  _resume = false;
+  while (_resume == false) {
+    if (Serial.available() > 0) {
+      char inByte = Serial.read();
+      if (inByte == 'y') {
+#if defined(ESP8266)|| defined(ESP32)
+        EEPROM.begin(512);
+#endif
+        EEPROM.put(calVal_eepromAdress, newCalibrationValue);
+#if defined(ESP8266)|| defined(ESP32)
+        EEPROM.commit();
+#endif
+        EEPROM.get(calVal_eepromAdress, newCalibrationValue);
+        Serial.print("Value ");
+        Serial.print(newCalibrationValue);
+        Serial.print(" saved to EEPROM address: ");
+        Serial.println(calVal_eepromAdress);
+        _resume = true;
+
+      }
+      else if (inByte == 'n') {
+        Serial.println("Value not saved to EEPROM");
+        _resume = true;
+      }
+    }
+  }
+
+  Serial.println("End calibration");
+  Serial.println("***");
+  Serial.println("To re-calibrate, send 'r' from serial monitor.");
+  Serial.println("For manual edit of the calibration value, send 'c' from serial monitor.");
+  Serial.println("***");
+}
+
+void changeSavedCalFactor() {
+  float oldCalibrationValue = LoadCell.getCalFactor();
+  boolean _resume = false;
+  Serial.println("***");
+  Serial.print("Current value is: ");
+  Serial.println(oldCalibrationValue);
+  Serial.println("Now, send the new value from serial monitor, i.e. 696.0");
+  float newCalibrationValue;
+  while (_resume == false) {
+    if (Serial.available() > 0) {
+      newCalibrationValue = Serial.parseFloat();
+      if (newCalibrationValue != 0) {
+        Serial.print("New calibration value is: ");
+        Serial.println(newCalibrationValue);
+        LoadCell.setCalFactor(newCalibrationValue);
+        _resume = true;
+      }
+    }
+  }
+  _resume = false;
+  Serial.print("Save this value to EEPROM adress ");
+  Serial.print(calVal_eepromAdress);
+  Serial.println("? y/n");
+  while (_resume == false) {
+    if (Serial.available() > 0) {
+      char inByte = Serial.read();
+      if (inByte == 'y') {
+#if defined(ESP8266)|| defined(ESP32)
+        EEPROM.begin(512);
+#endif
+        EEPROM.put(calVal_eepromAdress, newCalibrationValue);
+#if defined(ESP8266)|| defined(ESP32)
+        EEPROM.commit();
+#endif
+        EEPROM.get(calVal_eepromAdress, newCalibrationValue);
+        Serial.print("Value ");
+        Serial.print(newCalibrationValue);
+        Serial.print(" saved to EEPROM address: ");
+        Serial.println(calVal_eepromAdress);
+        _resume = true;
+      }
+      else if (inByte == 'n') {
+        Serial.println("Value not saved to EEPROM");
+        _resume = true;
+      }
+    }
+  }
+  Serial.println("End change calibration value");
+  Serial.println("***");
 }
